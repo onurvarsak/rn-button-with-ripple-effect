f7b0187b2a1e13cb7151c829f325567b67d808fb
onurvarsak <onurvarsak@outlook.com>
2025-01-06 08:03:50 +0300

    add text style

--- components/FlatButton.js
+++ components/FlatButton.js
@@ -4,22 +4,29 @@ import React from "react"
 const FlatButton = ({ children, onPress }) => {
   return (
     <Pressable onPress={onPress}>
-      <View style={styles.container}>
+      <View style={styles.buttonContainer}>
         {/* tüm stil özellikleri Text'e uygulanmadığı için View kullanıldı */}
-        <Text>{children}</Text>
+        <Text style={styles.textContainer}>{children}</Text>
       </View>
     </Pressable>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  buttonContainer: {
     paddingHorizontal: 16, // container'ın sağ ve sol kenarlarından 16 birim içeriye boşluk ekler
     paddingVertical: 10, // container'ın üst ve alt kenarlarından 10 birim içeriye boşluk ekler
     backgroundColor: "#FF6700",
     borderRadius: 5,
     elevation: 3, // android'de gölgelendirme efekti ekler,
     width: "100%"
+  },
+  textContainer: {
+    color: "#fff",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    fontSize: 16,
+    textAlign: "center"
   }
 })
 
