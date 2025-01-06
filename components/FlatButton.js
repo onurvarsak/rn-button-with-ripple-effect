2a5691fc50b0f7db5e10fd96bf4443965a7c0f3f
onurvarsak <onurvarsak@outlook.com>
2025-01-06 07:55:55 +0300

    add container style on FlatButton component

--- components/FlatButton.js
+++ components/FlatButton.js
@@ -1,15 +1,25 @@
-import { View, Text, Pressable } from "react-native"
+import { View, Text, Pressable, StyleSheet } from "react-native"
 import React from "react"
 
 const FlatButton = ({ children, onPress }) => {
   return (
     <Pressable onPress={onPress}>
-      <View>
+      <View style={styles.container}>
         {/* tüm stil özellikleri Text'e uygulanmadığı için View kullanıldı */}
         <Text>{children}</Text>
       </View>
     </Pressable>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16, // container'ın sağ ve sol kenarlarından 16 birim içeriye boşluk ekler
+    paddingVertical: 10, // container'ın üst ve alt kenarlarından 10 birim içeriye boşluk ekler
+    backgroundColor: "#FF6700",
+    borderRadius: 5,
+    elevation: 3 // android'de gölgelendirme efekti ekler
+  }
+})
 
 export default FlatButton
